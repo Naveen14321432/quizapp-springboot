98854f13834d144efe300fa2f9f3623423dc28fd
Naveen14321432 <naveenkammili04@gmail.com>
2025-03-25 13:19:20 +0530

    Added Repositories

--- src/main/java/com/example/quiz/repositories/AnswerRepository.java
+++ src/main/java/com/example/quiz/repositories/AnswerRepository.java
@@ -3,9 +3,9 @@ package com.example.quiz.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.quiz.model.User;
+import com.example.quiz.model.Answer;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long>{
+public interface AnswerRepository extends JpaRepository<Answer, Long> {
 
 }
--- src/main/java/com/example/quiz/repositories/QuestionRepository.java
+++ src/main/java/com/example/quiz/repositories/QuestionRepository.java
@@ -3,9 +3,9 @@ package com.example.quiz.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.quiz.model.User;
+import com.example.quiz.model.Question;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long>{
+public interface QuestionRepository extends JpaRepository<Question, Long>{
 
 }
